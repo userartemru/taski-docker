49640fc7a5e308657693d1a1d483068a315654b9
userartem <bochkov.artem1@yandex.ru>
2026-06-06 19:33:54 +0300

    deploy to chek

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '158.160.216.46', 'myhostname.serveirc.com']
+ALLOWED_HOSTS = ['localhost', '81.26.184.78', 'myhostname.serveirc.com']
 
 
 # Application definition
